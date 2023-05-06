d0e48253289dab48c151f8f439146865194234ee
josuerushanika <josuerushanika@gmail.com>
2023-05-06 03:56:30 +0200

    Update change

---
+++ car_project/explainRspec.rb
@@ -0,0 +1,15 @@
+.spec file   car_spec.rb                 
+.example group    describe
+.nested group    describe / context
+.example             it     
+.expectation       expect() .to ()   
+
+require 'car'
+
+describe 'Car' do
+    describe 'attributes' do--
+    describe '.colors' do--
+    describe '#full_name' do--        
+end
+
+video 24
